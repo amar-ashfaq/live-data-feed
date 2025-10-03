334c74dc451a9e21e049d7c8a88b646556ce5d1a
Amar AA <amar.ashfaq95@gmail.com>
2025-10-03 19:27:03 +0100

    Start implementing the live data background service.

---
+++ Services/LiveDataService.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LiveDataFeed.Services
+{
+    public class LiveDataService : BackgroundService
+    {
+        private readonly ILogger<LiveDataService> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly WebSockets.WebSocketHandler _webSocketHandler;
+        private readonly string _apiUrl = "http://dev-sample-api.tsl-timing.com/";
+
+        public LiveDataService(ILogger<LiveDataService> logger, IHttpClientFactory httpClientFactory, WebSockets.WebSocketHandler webSocketHandler)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+            _webSocketHandler = webSocketHandler;  
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
